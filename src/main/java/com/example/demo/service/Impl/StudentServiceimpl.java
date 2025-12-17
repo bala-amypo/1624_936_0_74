36866a3053fbcf22afe58f35c0e615a9973a2e32
bala-amypo <bala@amypo.in>
2025-12-17 09:14:12 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StudentServiceimpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentServiceimpl.java
@@ -3,7 +3,12 @@ import org.springframework.stereotype.service;
 @Service
 public class StudentServiceimpl implements StudentService{
         @Autowired StudentRepo student;
+        //save()
+        //findAll()
+        //findById()
+        //deleteById()
+        //existById()
          StudentEntity postData(StudentEntity stu){
-
+             
          }
 }
