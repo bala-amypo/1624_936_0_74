4e1ce4ced9503277a86ab5053fc703defbabded7
bala-amypo <bala@amypo.in>
2025-12-19 09:49:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentDetails.java
+++ src/main/java/com/example/demo/entity/StudentDetails.java
@@ -3,7 +3,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-@
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentDetails{
     @Id
     @GeneratedValue=(strategy=GenerationType.IDENITY)
---
+++ src/main/java/com/example/demo/entity/StudentID.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity;
+
+public class StudentID{
+    private Integer id;
+    private String idno;
+    private String stuid;
+}
